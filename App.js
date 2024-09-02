82f34da2b1858c3e01fc532eeb9b4b8d96cc14d2
sarabackstrom <sara.backstrom@myy.haaga-helia.fi>
2024-09-02 20:44:45 +0300

    valmis

--- App.js
+++ App.js
@@ -1,42 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, FlatList } from 'react-native';
 
 export default function App() {
 
-const buttonPressed = () => {
-  const summa = parseInt(arvo1) + parseInt(arvo2);
-  setTulos(summa.toString());
-}
-
-const buttonPressed2 = () => {
-  const erotus = parseInt(arvo1) - parseInt(arvo2);
-  setTulos(erotus.toString());
-}
-
-const [tulos, setTulos] = useState('Tähän tulee tulos');
+const [tulos, setTulos] = useState('');
 const [arvo1, setArvo1] = useState('');
 const [arvo2, setArvo2] = useState('');
+const [todos, setTodos] = useState([]);
 
-  return (
+const calculate = operator => {
+  const [num1, num2] = [Number(arvo1), Number(arvo2)];
+  let result;
+
+  switch (operator){
+    case '+':
+      result = num1 + num2;
+      setTulos(num1 + num2);
+      setTodos([...todos, {key: `${num1} + ${num2} = ${result}`}])
+      break;
+    case '-':
+      result = num1 - num2;
+      setTulos(num1 - num2);
+      setTodos([...todos, {key: `${num1} - ${num2} = ${result}`}])
+      break;
+  }
+  setArvo1('');
+  setArvo2('');
+}
+
+return (
 <View style={styles.container}>
 <Text>Result: {tulos}</Text>
-<TextInput 
- placeholder='Syötä arvo nro 1'
- onChangeText={arvo1 => setArvo1(arvo1)}
+<TextInput style={styles.input}
+ onChangeText={text1 => setArvo1(text1)}
  value={arvo1}
  keyboardType='numeric'  
 />
-<TextInput 
- placeholder='Syötä arvo nro 2'
- onChangeText={arvo2 => setArvo2(arvo2)}
+<TextInput style={styles.input}
+ onChangeText={text2 => setArvo2(text2)}
  value={arvo2}
  keyboardType='numeric' 
 />
-<View style={styles.buttonContainer}>
-<Button title='+' onPress={buttonPressed}/>
-<Button title='-' onPress={buttonPressed2}/>
+<View style={styles.operators}>
+<Button title='+' onPress={() => calculate('+')}/>
+<Button title='-' onPress={() => calculate('-')}/>
 </View>
+<Text>History: </Text>
+<FlatList 
+data={todos}
+renderItem={({item}) => <Text>{item.key}</Text>}/>
+<StatusBar style='auto'/>
     </View>
   );
 }
@@ -47,19 +61,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 60,
   },
   input: {
-    height: 40,
-    margin: 12,
+    borderColor: 'grey',
+    margin: 5,
     borderWidth: 1,
-    padding: 10,
+    padding: 5,
+    width: '50%'
   },
-  buttonContainer: {
+  operators: {
     flexDirection: 'row',
-    justifyContent: 'center',
-  },
-
-  button: {
-    marginHorizontal: 10,
+    justifyContent: 'space-evenly',
+    width: '50%'
   }
 });
